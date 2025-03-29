0f8149557a5baea8048b73d42e295c63ccab3bb8
VaishnaviSharma01 <166376679+VaishnaviSharma01@users.noreply.github.com>
2025-03-29 12:28:07 +0530

    Add files via upload

---
+++ StudentDetails1.java
@@ -0,0 +1,13 @@
+class StudentDetails1
+{
+public static void main(String args[])
+{
+String n1,n2,n3;
+n1=args[0];
+n2=args[1];
+n3=args[2];
+System.out.println("Name:"+n1);
+System.out.println("Roll-no:"+n2);
+System.out.println("Department:"+n3);
+}
+}
