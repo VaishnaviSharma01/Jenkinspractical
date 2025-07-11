6865d35518bcbec645a1b3a27bd52b58630df3e3
VaishnaviSharma01 <166376679+VaishnaviSharma01@users.noreply.github.com>
2025-07-11 13:15:52 +0530

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,14 @@
+class arithmetic
+{
+public static void main(String args[]){
+int n1=20,n2=10,r1,r2,r3,r4;
+r1=n1+n2;
+r2=n1-n2;
+r3=n1*n2;
+r4=n1/n2;
+System.out.println("Addition is"+r1);
+System.out.println("Substraction is"+r2);
+System.out.println("Multiplication is"+r3);
+System.out.println("Division is"+r4);
+}
+}
